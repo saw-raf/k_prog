11b1d6cdcb0e0329c1776b28d863e2fee64dd9a5
raf.station <rafals0707@gmail.com>
2017-03-12 14:49:28 +0100

    Problem 1 solved

---
+++ lab2/Test.java
@@ -0,0 +1,30 @@
+public class Test{
+	public static void main(String[] args){
+
+		try{
+			int rozmiar=Integer.parseInt(args[0]);
+			if(rozmiar<0)
+				throw new NegativeArraySizeException();
+			WierszTrojkataPascala wiersz=new WierszTrojkataPascala(rozmiar);
+			for(int i=1;i<args.length;i++){
+				try{
+					int wspolczynnik=Integer.parseInt(args[i]);
+					System.out.println(args[i]+": "+wiersz.wspolczynnik(wspolczynnik));
+				}
+				catch (NumberFormatException a){
+					System.out.println("\""+args[i]+"\""+" nie jest liczba");		
+				}
+				catch (ArrayIndexOutOfBoundsException b){
+					System.out.println(args[i]+": Wyszedles poza zakres");			
+				}
+			}
+						
+		}
+		catch (NegativeArraySizeException a){
+			System.out.println(args[0]+"; nieprawidłowy numer wiersza");	
+		}
+		catch (NumberFormatException e){
+			System.out.println("\""+args[0]+"\""+" nie jest liczba");		
+		}
+	}
+}
---
+++ lab2/WierszTrojkataPascala.java
@@ -0,0 +1,21 @@
+
+public class WierszTrojkataPascala{
+
+	private int rozmiar;
+	private int wiersz[];
+
+	public WierszTrojkataPascala(int n){
+		rozmiar=n+1;
+		wiersz=new int[rozmiar];
+		wiersz[0]=1;
+		for(int i=1;i<rozmiar;i++)
+			wiersz[i]=wiersz[i-1]*(n-(i-1))/i;//property C(n,k+1)=C(n,k)*(n-k)/(k+1)
+
+	}
+
+	public int wspolczynnik(int m) throws ArrayIndexOutOfBoundsException{
+		if(m>rozmiar || m<0)
+			throw new ArrayIndexOutOfBoundsException();	
+		return wiersz[m];
+	}
+}
