5296943ad315598e0c288ebe27927278b5fcbb04
RAFAL-PC WINDOWS <rapi2s@interia.pl>
2017-03-19 18:42:22 +0100

    small mistake repaired

--- lab3/Kwadrat.java
+++ lab3/Kwadrat.java
@@ -4,7 +4,7 @@ class Kwadrat extends Czworokat{
 	
 	Kwadrat(double a, double b, double c, double d, double k) throws NieKwadrat{ 
 	
-		if(kat!=90 || a!=b || b!=c || c!=d)
+		if(k=90 || a!=b || b!=c || c!=d)
 			throw new NieKwadrat();
 		kat=90.0;
 		bok1=bok2=bok3=bok4=a;
